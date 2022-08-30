d84f1446d744a196afb1e437dd908154d33e370c
Anil Natha <Anil.Natha@jpl.nasa.gov>
2022-08-30 11:21:39 -0700

    Added ability to read in config files that will dictate settings to be throughout the client library. Also included template for default config file, 'unity.cfg'. Users can copy this template, customize settings, and pass it in as a parameter when creating an instance of the Unity client.

--- unity_py/unity.py
+++ unity_py/unity.py
@@ -1,5 +1,21 @@
+import os
+from configparser import ConfigParser
+
+DEFAULT_CONFIG = os.path.dirname(os.path.realpath(__file__)) + "/unity.cfg"
+
+
 class Unity(object):
 
-    def __init__(self):
-        pass
-    
+    def __init__(self, user_config=None):
+        self._config = read_config([DEFAULT_CONFIG, user_config])
+
+
+def read_config(config_files):
+    config = ConfigParser()
+
+    for config_file in config_files:
+        if config_file is not None:
+            with open(config_file) as source:
+                config.read_file(source)
+
+    return config
