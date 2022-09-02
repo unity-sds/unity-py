7bdff6d11f139a91afca9e10c8b53e7d7066469f
mike-gangl <michael.e.gangl@jpl.nasa.gov>
2022-09-02 11:17:28 -0700

    added example test runs to documentation, added unity test example

--- tests/test_unity_py.py
+++ tests/test_unity_py.py
@@ -10,6 +10,10 @@ def cleanup_update_test():
     print("Cleanup...")
 
 
+def test_default_unity_client():
+    client = Unity()
+    assert True == True
+
 @pytest.mark.regression
 def test_example_regression_test(cleanup_update_test):
     print("Example regression test")
