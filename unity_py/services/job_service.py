b4258ab95bfbca273d0d99620c9530acdadf1355
Anil Natha <Anil.Natha@jpl.nasa.gov>
2023-01-03 10:57:55 -0800

    Renamed sps_processes config setting to sps_endpoint and updated associated references.

--- unity_py/services/job_service.py
+++ unity_py/services/job_service.py
@@ -5,9 +5,11 @@ from unity_py.utils.http import get_headers
 
 class JobService(object):
     """
-    The JobService class is a wrapper to the job endpoint(s) within Unity. This wrapper interfaces with the Science Processing Service endpoints.
+    The JobService class is a wrapper to the job endpoint(s) within Unity.
+    This wrapper interfaces with the Science Processing Service endpoints.
 
-    The JobService class allows for the querying and deploying of processes. In addition, the querying, execution, monitoring of jobs and the job information retrieval.
+    The JobService class allows for the querying and deploying of processes.
+    In addition, the querying, execution, monitoring of jobs and the job information retrieval.
     """
 
     def __init__(
@@ -30,18 +32,18 @@ class JobService(object):
         """
         self._session = session
         if endpoint is None:
-            self.endpoint = self._session.get_service_endpoint("jobs", "sps_processes")
+            self.endpoint = self._session.get_service_endpoint("jobs", "sps_endpoint")
 
     def get_processes(self):
         """
         Returns a list of processes already deployed within SPS
         """
-    
-        url = self.endpoint
+
+        url = self.endpoint + "processes"
         token = self._session.get_auth().get_token()
         headers = get_headers(token)
         response = requests.get(url, headers=headers)
         response.raise_for_status()
         json_result = response.json()['processes']
-        
+
         return json_result
