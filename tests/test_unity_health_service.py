70941c5c2340b8b1cd350c5258c3bf2f3098c73e
Anil Natha <Anil.Natha@jpl.nasa.gov>
2024-06-17 15:46:36 -0700

    Removed uneeded health service unit test fixture.

--- tests/test_unity_health_service.py
+++ tests/test_unity_health_service.py
@@ -9,13 +9,8 @@ from unity_sds_client.unity import Unity
 from unity_sds_client.unity_services import UnityServices
 
 
-@pytest.fixture
-def cleanup_update_test():
-    yield None
-    print("Cleanup...")
-
 @pytest.mark.regression
-def test_health_service_client_creation(cleanup_update_test):
+def test_health_service_client_creation():
     """
     Test that an instance of the health service can be instantiated.
     """
@@ -23,7 +18,7 @@ def test_health_service_client_creation(cleanup_update_test):
     health_service = s.client(UnityServices.HEALTH_SERVICE)
 
 @pytest.mark.regression
-def test_health_status_retrieval(cleanup_update_test):
+def test_health_status_retrieval():
     """
     Test that health statuses can be retrieved using the health service.
     """
@@ -34,7 +29,7 @@ def test_health_status_retrieval(cleanup_update_test):
     assert health_statuses is not None
 
 @pytest.mark.regression
-def test_health_status_printing(cleanup_update_test):
+def test_health_status_printing():
     """
     Test that health statuses can be printed using the health service.
     """
